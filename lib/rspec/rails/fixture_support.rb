6818a5b45b122e29e77e5b3e8cdebaa0057d3fc0
Will Bryant <will.bryant@gmail.com>
2015-09-17 12:01:57 +1200

    add support for the rails pre_loaded_fixtures config setting

--- lib/rspec/rails/fixture_support.rb
+++ lib/rspec/rails/fixture_support.rb
@@ -24,6 +24,7 @@ module RSpec
           self.fixture_path = RSpec.configuration.fixture_path
           self.use_transactional_fixtures = RSpec.configuration.use_transactional_fixtures
           self.use_instantiated_fixtures  = RSpec.configuration.use_instantiated_fixtures
+          self.pre_loaded_fixtures        = RSpec.configuration.pre_loaded_fixtures
           fixtures RSpec.configuration.global_fixtures if RSpec.configuration.global_fixtures
         end
       end
